972d2b4878350d5d23a1d44cf877ee45f5a2f39b
Renato Silva <orenatodos@gmail.com>
2022-01-16 17:45:20 -0300

    feat: configure babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle[hash].js'
@@ -13,5 +13,14 @@ module.exports = {
       template: path.resolve(__dirname, 'public', 'index.html')
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
 }
